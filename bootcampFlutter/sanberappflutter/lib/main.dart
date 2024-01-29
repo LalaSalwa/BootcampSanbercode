648ad1d32f09ec27a5bd4e6fcdd54aac28142d2a
LalaSalwa <lalasalwactk@gmail.com>
2024-01-29 23:10:02 +0700

    Tugas11

--- bootcampFlutter/sanberappflutter/lib/main.dart
+++ bootcampFlutter/sanberappflutter/lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
-import 'tugas/tugas9/telegram.dart';
+import 'package:sanberappflutter/tugas/tugas11/Homescreen.dart';
+import 'package:sanberappflutter/tugas/tugas11/login.dart';
+// import 'tugas/tugas10/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(
+  ));
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Telegram(),
+      home: LoginScreen(),
+      routes: <String, WidgetBuilder>{
+      '/login': (context) => LoginScreen(),
+      '/home': (context) => HomeScreen(),
+    },
     );
   }
 }
--- bootcampFlutter/sanberappflutter/lib/tugas/tugas11/login.dart
+++ bootcampFlutter/sanberappflutter/lib/tugas/tugas11/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sanberappflutter/tugas/tugas10/Homescreen.dart';
+import 'Homescreen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
