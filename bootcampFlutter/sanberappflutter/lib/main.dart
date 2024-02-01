e3a01e786f0e5a189c053925f2de70ce6058e63f
LalaSalwa <lalasalwactk@gmail.com>
2024-02-01 22:59:37 +0700

    update_tugas13

--- bootcampFlutter/sanberappflutter/lib/main.dart
+++ bootcampFlutter/sanberappflutter/lib/main.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:sanberappflutter/tugas/tugas12/get_data.dart';
-import 'package:sanberappflutter/tugas/tugas12/get_detail_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sanberappflutter/tugas/tugas13/loginscreen.dart';
+// import 'package:sanberappflutter/tugas/tugas12/get_data.dart';
+// import 'package:sanberappflutter/tugas/tugas12/get_detail_screen.dart';
 // import 'package:sanberappflutter/tugas/tugas11/Homescreen.dart';
 // import 'package:sanberappflutter/tugas/tugas11/login.dart';
 // import 'tugas/tugas10/login.dart';
 
-void main() {
-  runApp(const MyApp(
-  ));
+// void main() {
+//   runApp(const MyApp(
+//   ));
+// }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: GetDataScreen(),
+//     //   routes: <String, WidgetBuilder>{
+//     //   '/login': (context) => LoginScreen(),
+//     //   '/home': (context) => HomeScreen(),
+//     // },
+//     );
+//   }
+// }
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter demo',
       debugShowCheckedModeBanner: false,
+      title: 'Flutter demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GetDataScreen(),
-    //   routes: <String, WidgetBuilder>{
-    //   '/login': (context) => LoginScreen(),
-    //   '/home': (context) => HomeScreen(),
-    // },
+      home: LoginScreen(),
     );
   }
 }
-
