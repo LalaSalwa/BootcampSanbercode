66aa218412230103f3594be9ed7645d162b51f51
LalaSalwa <lalasalwactk@gmail.com>
2024-02-02 21:14:49 +0700

    update_tugas14

--- bootcampFlutter/sanberappflutter/lib/main.dart
+++ bootcampFlutter/sanberappflutter/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:sanberappflutter/tugas/tugas13/loginscreen.dart';
+import 'package:sanberappflutter/tugas/tugas14/get_data_scree.dart';
 // import 'package:sanberappflutter/tugas/tugas12/get_data.dart';
 // import 'package:sanberappflutter/tugas/tugas12/get_detail_screen.dart';
 // import 'package:sanberappflutter/tugas/tugas11/Homescreen.dart';
@@ -32,22 +32,46 @@ import 'package:sanberappflutter/tugas/tugas13/loginscreen.dart';
 //   }
 // }
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+// Future<void> main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: LoginScreen(),
+//     );
+//   }
+// }
+
+void main() {
+  runApp(const MyApp(
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: GetDataScreenStateManagament() ,
+    //   routes: <String, WidgetBuilder>{
+    //   '/login': (context) => LoginScreen(),
+    //   '/home': (context) => HomeScreen(),
+    // },
     );
   }
 }
---
+++ bootcampFlutter/sanberappflutter/lib/tugas/tugas14/controller/appcontroller.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+import 'package:sanberappflutter/tugas/tugas14/services/services.dart';
+import 'package:get/get.dart';
+import 'package:sanberappflutter/tugas/tugas14/model/postmode.dart';
+
+class AppController extends GetxController {
+  var getposts = <Postmodel>[].obs;
+  Services services = Services();
+  var postloading = true.obs;
+  @override
+  void onInit(){
+    callpostmethod();
+    super.onInit();
+  }
+  callpostmethod() async{
+    try{
+      postloading.value = true;
+      var result = await services.getallposts();
+      if (result != null){
+        getposts.assignAll(result);
+      } else{
+        print("null");
+      }
+    } finally {
+      postloading.value = false;
+    }
+    update();
+  }
+}
